7c6648686c9717087d15f1d299b5a82feeb0c7d6
Mauro Santos Lopes <Mauro.slopes06@gmail.com>
2024-05-03 16:58:17 -0300

    68 - Destrutores

---
+++ pooDestrutores/pooDestrutores/Ferramentas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestrutores
+{
+    internal class Ferramentas
+    {
+        public Ferramentas(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        ~Ferramentas()
+        {
+            Console.WriteLine("Teste");
+        }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ pooDestrutores/pooDestrutores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ferramentas f1 = new Ferramentas("Martelo");
+            Ferramentas f2 = new Ferramentas("Alicate");
+
+            f1 = null;
+            f1 = new Ferramentas("Marreta");
+
+            GC.Collect();
+
+            Console.ReadLine();
+        }
+    }
+}
